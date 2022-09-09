f68d23e8a2532eeab1d0233f6398ec033bd85909
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-09 11:58:17 -0700

    Code that check if a number is negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+* random_number - get square of number passed
+*@n: the random number to check
+*
+*Return: is positive or is zero or is negative
+*/
 int main(void)
 {
 	int n;
@@ -13,7 +19,7 @@ int main(void)
 	if(n>0){
 		printf("is positive");
 	}else if(n == 0){
-		printf("is zero");
+		printf("0 is zero");
 	}else{
 		printf("is negative");
 	}
