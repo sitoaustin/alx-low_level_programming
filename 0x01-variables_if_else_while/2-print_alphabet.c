e55d4cdd9bad7ae6b90edc445464681d0b2deb1a
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-09 15:07:44 -0700

    Program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main initialization
+ *
+ * Return: a-z aphabets
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ch++)
+
+		putchar(ch);
+
+	return (0);
+}
