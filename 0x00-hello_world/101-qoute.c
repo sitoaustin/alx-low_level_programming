f7f9fa2813a752a5d5f56a31577ebfc298524c5e
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-11 12:48:05 -0700

    A script that prints to the standard error in c

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,14 @@
+table File  12 lines (11 sloc)  185 Bytes
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
