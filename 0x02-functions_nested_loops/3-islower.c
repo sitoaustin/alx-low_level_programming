193e09ce7ec6d4616f75e4fbd0327e55b8b61b1e
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-13 07:55:58 -0700

    Fuction that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if c is lowercase
+ *
+ * Return: void
+ */
+
+int print_alphabet(int c)
+{
+	if (c == 99)
+	{
+		return (1);
+	} 
+	else
+	{
+		return (0);
+	}
+}
