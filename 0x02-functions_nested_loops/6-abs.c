9d8fbdb0d1b50989f512fac235e7226ca2bdabff
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-13 09:04:11 -0700

    A function that prints the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @r: The character in ASCII code
+ *
+ * Return: void
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r *= -1;
+		printf("%d", r);
+	}
+	else
+	{
+		printf("%d", r);
+	}
+}
