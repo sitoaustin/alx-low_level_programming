1a41d753246c07a206fe9ac288df066fbbe2050e
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-15 08:44:51 -0700

    Checks is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int _isupper - Checks if c is upper or lower
+ *
+ * @c: takes an int
+ *
+ * Return: 1 if c is uppercase
+ * and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c == 99)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
