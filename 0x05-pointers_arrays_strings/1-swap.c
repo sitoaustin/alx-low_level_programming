9cbaf09bce65d4b8f14a81f87bf232e9804f32d3
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-19 06:39:49 -0700

    a function that swaps the values of two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - draw a diagonal line
+ * @a: int
+ * @b: int
+ * Description: Can only use _putchar to print
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
