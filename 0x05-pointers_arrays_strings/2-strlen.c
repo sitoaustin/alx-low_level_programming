d67d844580b76a88d809ae153d758af743d66e79
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-19 10:45:44 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ * Description: Can only use _putchar to print
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = (sizeof(*s) / sizeof(moon[0]));
+	
+	return(len);
+}
