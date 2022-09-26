28865f147e41fb95588ac1da419fc3d2eb8c37a7
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-26 07:00:41 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fuction that fills the first n
+ * bytes of the memory area pointed to by s...
+ *
+ * @s: a pointer
+ * @b: a pointer
+ * @n: an unsigned int
+ *
+ * Return: a character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	printf("moon");
+	return (s);
+}
