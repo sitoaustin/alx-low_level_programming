9f5e95f07c15fc3de3a58d097cf77977abb594fc
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-30 03:06:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a function that prints out the name
+ * of the file
+ *
+ * @argc: an integer that holds the number of
+ * items being passed into...
+ * @argv: an array that hold all the items that's being
+ * passed into..
+ *
+ * RETURN: void
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
