d269b910e4c1587024cbce486a3607874d7e4fbe
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-09-30 05:01:55 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - a function that prints out the name
+ * of the file
+ *
+ * @argc: an integer that holds the number of
+ * items being passed into...
+ * @argv: an array that hold all the items that's being
+ * passed into..
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
