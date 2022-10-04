980b99fc1d4d5470a3120f2fc852b9f7e596e149
Nakachukwu Sixtus Onyedibe <67762676+Xys-Tus@users.noreply.github.com>
2022-10-04 22:31:33 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,23 +12,31 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *c_cant;
-	int i;
-
-	c_cant = malloc(sizeof(char) * 3);
-
-	i = 0;
-	
-	if ((s1 == 0) & (s2 == 0))
+	char *strDup;
+	int i, j;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	strDup = malloc(sizeof(char) * (i + j + 1));
+	if (strDup == NULL)
 		return (NULL);
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
-		c_cant[i] = s1[i];
+		strDup[i] = s1[i];
 		i++;
 	}
-	while (s2[i] != '\0')
+	while (s2[j] != '\0')
 	{
-		c_cant[i] = s2[i];
+		strDup[i] = s2[j];
+		i++, j++;
 	}
-	return (c_cant);
+	strDup[i] = '\0';
+	return (strDup);
 }
