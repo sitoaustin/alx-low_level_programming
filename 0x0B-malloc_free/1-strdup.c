113e54b3fea56cf1bc4c759a2d9d3bbc5135bdea
Nakachukwu Onyedibe <onyedibesixtusna@gmail.com>
2022-10-04 08:46:58 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,15 +13,23 @@
 
 char *_strdup(char *str)
 {
-	char *mem;
-	int sizeof_str = sizeof(str);
-	int size = sizeof(str[0]);
-	int i = (sizeof_str / size) - 1;
-
-	mem = malloc(sizeof(str) * i);
+	char *strDup;
+	int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	return (mem);
-
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	strDup = malloc(sizeof(*str) * i);
+	if (strDup == NULL)
+		return (NULL);
+	j = 0;
+	while (str[j] != '\0')
+	{
+		strDup[j] = str[j];
+		j++;
+	}
+	return (strDup);
 }
